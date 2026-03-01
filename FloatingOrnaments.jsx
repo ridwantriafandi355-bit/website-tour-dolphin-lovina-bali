dda96f0ccaafd8632dda0b0a4b499c7f0acdfc0c
Developer <developer@example.com>
2026-03-01 11:19:24 +0700

    Remove the bottommost ornament image

--- FloatingOrnaments.jsx
+++ FloatingOrnaments.jsx
@@ -21,28 +21,22 @@ const FloatingOrnaments = () => {
         
         Or you can use the built-in 'animate-bounce' if preferred.
       */}
-      
+
       {/* Ornament 1 */}
-      <img 
-        src="/ornamen bali png/bali.png" 
-        alt="Ornamen Bali 1" 
+      <img
+        src="/ornamen bali png/bali.png"
+        alt="Ornamen Bali 1"
         className="w-32 h-auto opacity-85 animate-[float_6s_ease-in-out_infinite]"
-        // Using arbitrary value animate-[...] as a fallback if tailwind config isn't updated
+      // Using arbitrary value animate-[...] as a fallback if tailwind config isn't updated
       />
 
       {/* Ornament 2 */}
-      <img 
-        src="/ornamen bali png/daun kelapa emas.png" 
-        alt="Ornamen Bali 2" 
+      <img
+        src="/ornamen bali png/daun kelapa emas.png"
+        alt="Ornamen Bali 2"
         className="w-32 h-auto opacity-85 animate-[float_7s_ease-in-out_infinite_animation-delay-1000ms]"
       />
 
-      {/* Ornament 3 */}
-      <img 
-        src="/ornamen bali png/siluet ombak minimalis.png" 
-        alt="Ornamen Bali 3" 
-        className="w-32 h-auto opacity-85 animate-[float_5s_ease-in-out_infinite_animation-delay-2000ms]"
-      />
     </div>
   );
 };
